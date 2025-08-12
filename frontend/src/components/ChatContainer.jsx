2c37a2974326384aac040958fe3f43636918b43c
Ashvita-6 <r.ashvita.mec22@itbhu.ac.in>
2025-08-12 15:51:55 +0530

    error fix

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,4 +1,4 @@
-import { useChatStore } from "../store/useChatstore";
+import { useChatStore } from "../store/useChatStore";
 import { useEffect, useRef } from "react";
 
 import ChatHeader from "./ChatHeader";
